576daafb37f9c8737ff1a1aba3ed49b993e721d0
OneMoreLight <88867522+OneMoreLight28th@users.noreply.github.com>
2023-08-15 23:00:14 +0800

    test

--- src/main/java/com/inspur/mapreduce/championcnt/test.java
+++ src/main/java/com/inspur/mapreduce/championcnt/test.java
@@ -1,6 +1,10 @@
 package com.inspur.mapreduce.championcnt;
 
 public class test {
+
+    public String hello(){
+        return "hello";
+    }
     public static void main(String[] args) {
         System.out.println("hello world");
     }
